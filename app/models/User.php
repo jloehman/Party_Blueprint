fbdf3060e4dd057972e350fb90999ee322b833ea
malloryweatherston <mjw0909@sbcglobal.net>
2014-07-15 17:06:11 -0500

    Added models for Budget, Guest, Party, Todo, and User.

--- app/models/User.php
+++ app/models/User.php
@@ -23,4 +23,17 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password', 'remember_token');
 
+
+	public function parties()
+	{
+    	return $this->hasMany('Party');
+	}
+
+	//In production mode, don't hash passwords
+
+	// public function setPasswordAttribute($value)
+	// {
+ //   		$this->attributes['password'] = Hash::make($value);
+	// }
+
 }
