3bc1e1c9575ce0706813c279955e161d19746c4f
JLo <jkloehman@jasons-air.attlocal.net>
2014-07-14 20:10:01 -0500

    Databases with user seeder

--- app/database/migrations/2014_07_12_184517_create_users_table.php
+++ app/database/migrations/2014_07_12_184517_create_users_table.php
@@ -13,13 +13,16 @@ class CreateUsersTable extends Migration {
 	public function up()
 	{
 		Schema::create('users', function($table)
-			{
+		{
 			$table->increments('id');
+			$table->boolean('is_admin');
 		    $table->string('first_name', 20);
 			$table->string('last_name', 20);
 		    $table->string('email', 200)->unique();
 		    $table->string('password', 100);
-			});
+
+		    $table->timestamps();
+		});
 	}
 
 	/**
