499a21cc9766cc4b365840c8f17a1aa6f92814ce
Lindsey Neubauer <lp.neubauer@gmail.com>
2014-07-16 10:34:08 -0500

    Created table on the To Do List view and created a route on the master page to link to the To Do List view.

--- app/views/pages_folder/todo_list.blade.php
+++ app/views/pages_folder/todo_list.blade.php
@@ -1,4 +1,21 @@
 @extends('layouts.user_master')
 @section('content')
 <h1>To Do List Page</h1>
+<div class="table-responsive">
+  <table class="table table-bordered table-striped">
+  	<tr>
+  		<th>Title</th>
+  		<th>Quantity</th>
+  		<th>Actions</th>
+  	</tr>
+
+  	<tr>
+  		<td></td>
+  		<td></td>
+  		<td></td>
+  	</tr>
+
+  </table>
+</div>
+
 @stop
