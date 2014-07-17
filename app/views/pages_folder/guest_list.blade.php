2e81998a667498c0a5e819005952c437476cd876
JLo <jkloehman@gmail.com>
2014-07-17 10:51:31 -0500

    guest list form, veiwed in routes but did not change anything

--- app/views/pages_folder/guest_list.blade.php
+++ app/views/pages_folder/guest_list.blade.php
@@ -1,4 +1,79 @@
 @extends('layouts.user_master')
+
 @section('content')
 <h1>Guest List Page</h1>
+<div class="table-responsive">
+  <table class="table table-bordered table-striped">
+  	<tr>
+  		<th>Name</th>
+  		<th>Email</th>
+  		<th>Phone</th>
+  		<th>RSVP</th>
+  		<th>Notes</th>
+  		<th></th>
+  	</tr>
+@foreach($guests as $guest)
+  	<tr>
+  		<td>{{{ $guest->name }}}</td>
+  		<td>{{{ $guest->email }}}</td>
+  		<td>{{{ $guest->phone }}}</td>
+  		<td>{{{ $guest->is_attending }}}</td>
+
+  		<!-- this needs to be a checkbox -->
+  		<td>
+  			<!-- this needs to be submitted -->
+  			{{ Form::checkbox('rsvp')}}
+
+  		</td>
+  		<td>{{{ $guest->is_attending }}}</td>
+  		<td>{{{ $guest->comment }}}</td>
+  		<td>{{ Form::open(array('action' => array('PartyController@destroy', $todo->id), 'method' => 'DELETE' )) }}
+			{{ Form::submit('Delete') }}
+			{{ Form::close() }}
+		</td>
+
+  	</tr>
+@endforeach
+  </table>
+</div>
+
+
+	{{-- Form::submit('Add Guests') --}}
+
+
+
+	{{ Form::open(array('action' => 'PartyController@store')) }}
+  <div>
+		{{ Form::label('name', 'Name') }}<br>
+		{{ Form::text('name', Input::old('name')) }}<br>
+		<!-- Change this error message -->
+		{{ $errors->first('name', '<span class="help-block">:message</span><br>') }}
+	</div>
+	<div>
+		{{ Form::label('email', 'Email') }}<br>
+		{{ Form::text('email', null, array('placeholder' => 'email')) }}<br>
+		{{ $errors->first('email', '<span class="help-block">:message</span><br>') }}
+	</div>
+	<div>
+		{{ Form::label('phone', 'Phone') }}<br>
+		{{ Form::text('phone', null, array('placeholder' => '###-###-####')) }}<br>
+		{{ $errors->first('phone', '<span class="help-block">:message</span><br>') }}
+	</div>
+	<div>
+		{{ Form::label('is_attending', 'is_attending') }}<br>
+		{{ Form::text('is_attending', null, array('placeholder' => 'RSVP')) }}<br>
+		{{ $errors->first('is_attending', '<span class="help-block">:message</span><br>') }}
+	</div>
+	<div class="form-group">
+		{{ Form::label('comment', 'Comment') }}
+			<div class="wmd-panel">
+            <div id="wmd-button-bar"></div>
+            <textarea class="wmd-input" id="wmd-input"></textarea>	
+         {{ $errors->first('comment', '<span class="help-block">:message</span><br>') }}
+         	</div>
+	</div>
+	
+		{{ Form::submit('Add Guest') }}
+		{{ Form::close() }}
+
 @stop
