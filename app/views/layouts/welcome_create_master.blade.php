f4c6b0fb51a71456ce5a7e34d4797348206b09af
Lindsey Neubauer <lp.neubauer@gmail.com>
2014-07-21 12:38:52 -0500

    Updated welcome blade to include the formatting to link to a previous party made or create a new one. Created the welcome_create_master blade for formatting with the welcome blade and create_new_party blade. Created the create_new_party blade that has a form to input the specs of a party, the budget and create a party button.

---
+++ app/views/layouts/welcome_create_master.blade.php
@@ -0,0 +1,13 @@
+
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+@yield('topscript')
+
+
+@yield('content')
+
+	<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="assets/js/jquery-func.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/retina.js"></script>
+	<script type="text/javascript" src="assets/js/smoothscroll.js"></script>
+@yield('bottomscript')
--- app/views/pages_folder/welcome.blade.php
+++ app/views/pages_folder/welcome.blade.php
@@ -1,3 +1,26 @@
 <!--This needs to be a redirect through the controllers.
 	This page will have a button that does the redirecting
 	to the personal admin page of the user-->
+@extends('layouts.welcome_create_master')
+@section('content')
+<div class="container">
+	<div class="row">
+		<h1 class="welcome">Welcome!</h1>
+	</div>
+	<div class="create_button btn-lg">
+		<a href="{{{ action('WelcomeController@create') }}}" class="btn btn-success">Create New Party</a>
+	</div>
+	<div class="choose_party btn-group">
+	    <button class="btn btn-default btn-lg dropdown-toggle" type="button" data-toggle="dropdown">
+	        Choose a Party <span class="caret"></span>
+	    </button>
+
+	  <div class="dropdown-menu" role="menu">
+	  	@foreach($parties as $party)
+	  	<!-- when one of these is chosen, I need it to render the specific information -->
+	        <a href="{{{ action('PartyController@show', $party->id) }}} class="list-group-item"">{{{ $party->party_name }}}</a>
+		@endforeach
+	  </div>
+	</div>
+</div>
+@stop
