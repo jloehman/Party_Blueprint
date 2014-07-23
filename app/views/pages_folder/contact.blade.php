d97f1d224bcb64641a6206c81d5526cf29ba8055
JLo <jkloehman@gmail.com>
2014-07-23 15:51:45 -0500

    uodated contact blade to laravel to match with Lindsey email send

--- app/views/pages_folder/contact.blade.php
+++ app/views/pages_folder/contact.blade.php
@@ -2,50 +2,37 @@
 
 @section('content')
 
-<div class="container">
-<div class="row">
-  <div class="col-md-12">
-    <small><i></i>Message sent</i></small>
-	<div class="alert alert-success"><strong><span class="glyphicon glyphicon-send"></span> Message sent </strong></div>	  
-    <div class="alert alert-danger"><span class="glyphicon glyphicon-alert"></span><strong> Message not sent</strong></div>
-  </div>
-  <form role="form" action="" method="post" >
-    <div class="col-lg-6">
-      <div class="well well-sm"><strong><i class="glyphicon glyphicon-ok form-control-feedback"></i> Required Field</strong></div>
-      <div class="form-group">
-        <label for="InputName">Your Name</label>
-        <div class="input-group">
-          <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Enter Name" required>
-          <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
-      </div>
-      <div class="form-group">
-        <label for="InputEmail">Your Email</label>
-        <div class="input-group">
-          <input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="Enter Email" required  >
-          <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
-      </div>
-      <div class="form-group">
-        <label for="InputMessage">Message</label>
-        <div class="input-group"
->
-          <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" required></textarea>
-          <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
-      </div>
-      <div class="form-group">
-        <label for="InputReal">What is 4+3?</label>
-        <div class="input-group">
-          <input type="text" class="form-control" name="InputReal" id="InputReal" required>
-          <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
-      </div>
-      <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-info pull-right">
-    </div>
-  </form>
-  <hr class="featurette-divider hidden-lg">
-  <div class="col-lg-5 col-md-push-1">
-    
-  </div>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+ 
+   </head>
+ <body>
+ //unsure on which controller to call
+   {{ Form::open(array('action' => 'HomeController@doContact')) }}
+        {{ Form::label('name', 'name:')}}
+        {{ Form::text('name',null, array('class' => 'form-control'))}}
+        <br>
+        {{ Form::label('email', 'email')}}
+        {{ Form::text('email',null, array('class' => 'form-control'))}}
+        <br>
+        {{ Form::label('message', 'Message::')}}
+        {{ Form::textarea('message',null, array('class' => 'form-control'))}}
+        
+        <br>
+        <button type="Submit" class="btn btn-default">Log In</button>
 
-</div>
+            
 
+        {{ Form::close() }}
+     
+
+  </body>
+</html>
 @stop
+
+
+
+labels:
+
