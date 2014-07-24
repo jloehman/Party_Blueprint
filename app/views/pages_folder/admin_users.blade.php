573839e7de690a247faf90e7609dd14dec1cbcad
malloryweatherston <mjw0909@sbcglobal.net>
2014-07-24 15:29:29 -0500

    Formatted Admin pages and applied new theme.

--- app/views/pages_folder/admin_users.blade.php
+++ app/views/pages_folder/admin_users.blade.php
@@ -2,23 +2,36 @@
 
 @section('content') 
 
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-
-  </head>
-
-  <body>
-  			<h1>Users</h1>
+ <section id="page-title" class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h3>USER MANAGEMENT</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+      
+      
+    <!--Content-->      
+    <section id="content2" class="section">
+      <div class="container">
+       <!--  <div class="row"> -->
+          
+          <!--Column One-->
+          <div class="col-sm-12 text-center">
+            
+            <!--Blog Post-->
+            <article class="margin-60">
+  			<h3>Existing Users</h3><br>
 			<div class="table-responsive">
 	  			<table class="table table-bordered table-striped">
 		  	<tr>
-		  		<th>First Name</th>
-		  		<th>Last Name</th>
-		  		<th>Email</th>
-		  		<th>Creation Date</th>
+		  		<th class="text-center">First Name</th>
+		  		<th class="text-center">Last Name</th>
+		  		<th class="text-center">Email</th>
+		  		<th class="text-center">Creation Date</th>
+		  		<th class="text-center">Action</th>
 		  	</tr>
 			@foreach($users as $user)
 			@if($user->is_admin == false)
@@ -37,5 +50,15 @@
 			@endforeach
 		  </table>
 		</div>
+
+		</article>
+            
+        </div><!--End Column One-->
+          
+        </div><!--End Row-->
+            
+                 
+      <!-- </div> -->
+    </section>
 
 @stop
