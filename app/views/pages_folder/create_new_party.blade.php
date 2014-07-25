eef3aec163008e461065ddc6a0dac5ba10477e1d
Lindsey Neubauer <lp.neubauer@gmail.com>
2014-07-25 18:50:00 -0500

    Updated the user_master blade to include a datetimepicker script. Modified the create_new_party blade for viewing. Implemented jQuery plugin the todo_list for picking a date. Updated the welcome.css file.

--- app/views/pages_folder/create_new_party.blade.php
+++ app/views/pages_folder/create_new_party.blade.php
@@ -7,13 +7,13 @@
 		<div class="steps">
 			<div class="row_two">
 				<div class="step_one">
-					<p><em>1. Set Party Attributes</em></p>
+					<p><b><em>1. Set Party Attributes</em></b></p>
 				</div>
 				<div class="step_two">
-					<p><em>2. Set Your Budget</em></p>
+					<p><b><em>2. Set Your Budget</em></b></p>
 				</div>
 				<div class="step_three">
-					<p><em>3. Click Create!</em></p>
+					<p><b><em>3. Click Create Party</em></b></p>
 				</div>
 			</div>
 		</div>
@@ -22,34 +22,34 @@
 			<div class="row_three">
 				{{ Form::open(array('action' => 'WelcomeController@store', "class" => "form-horizontal form-group")) }}
 			  	<div class="specs">
-					{{ Form::label('party_name', 'Party Name') }}<br>
+					{{ Form::label('party_name', 'Party Name', array('class' => 'party_attributes')) }}<br>
 					{{ Form::text('party_name', Input::old('party_name'), array('placeholder'=>'Party Name', 'class' => 'form-control')) }}<br>
 					<!-- Change this error message -->
 					{{ $errors->first('name', '<span class="help-block">:message</span><br>') }}
 
-					{{ Form::label('location', 'Location') }}<br>
+					{{ Form::label('location', 'Location', array('class' => 'party_attributes')) }}<br>
 					{{ Form::text('location', Input::old('location'), array('placeholder'=>'Location', 'class' => 'form-control')) }}<br>
 					<!-- Change this error message -->
 					{{ $errors->first('location', '<span class="help-block">:message</span><br>') }}
 
-					{{ Form::label('event_date', 'Event Date') }}<br>
+					{{ Form::label('event_date', 'Event Date', array('class' => 'party_attributes')) }}<br>
 					{{ Form::text('event_date', Input::old('event_date'), array('placeholder'=>'YYYY-MM-DD', 'class' => 'form-control', 'id' => "datetimepicker")) }}<br>
 					<!-- Change this error message -->
 					{{ $errors->first('event_date', '<span class="help-block">:message</span><br>') }}
 
-					{{ Form::label('start_time', 'Event Time') }}<br>
+					{{ Form::label('start_time', 'Event Time', array('class' => 'party_attributes')) }}<br>
 					{{ Form::text('start_time', Input::old('start_time'), array('placeholder'=>'00:00:00', 'class' => 'form-control', 'id' => 'datetimepicker2')) }}<br>
 					<!-- Change this error message -->
 					{{ $errors->first('start_time', '<span class="help-block">:message</span><br>') }}
 				</div>
 				<div class="set_budget">
-					{{ Form::label('budget', 'Budget') }}<br>
-					{{ Form::text('budget', Input::old('budget'), array('placeholder'=>'EX: 0.00', 'class' => 'form-control')) }}<br>
+					{{ Form::label('budget', 'Budget', array('class' => 'party_attributes')) }}<br>
+					{{ Form::text('budget', Input::old('budget'), array('placeholder'=>'ex: 0.00', 'class' => 'form-control')) }}<br>
 					<!-- Change this error message -->
 					{{ $errors->first('budget', '<span class="help-block">:message</span><br>') }}
 				</div>
 				<div class="create_button">
-					{{ Form::submit('Create Party', array('class' => 'btn')) }}
+					{{ Form::submit('Create Party', array('id' => 'create_party', 'class' => 'btn')) }}
 					{{ Form::close() }}
 				</div>
 			</div>
@@ -62,7 +62,7 @@
 <script>
 
 	$('#datetimepicker').datetimepicker({
-		startDate: new Date(),
+		startDate: 0,
 		format: 'Y/m/d',
 		timepicker: false
 	});
